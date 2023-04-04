1e5e30b5f643ed99418c4fccfe347898cf574ca9
Rahul chakraborty <rahulc0dy@C0DY>
2023-04-04 20:57:00 +0530

    completed sign in page aka landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,16 +16,6 @@ export default function Home() {
                     content="width=device-width, initial-scale=1"
                 />
                 <link rel="icon" href="Top-logo.png" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossorigin
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;700;900&display=swap"
-                    rel="stylesheet"
-                />
             </Head>
             <Layout>
                 <LandingPage />
